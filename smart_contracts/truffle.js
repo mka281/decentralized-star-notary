a4656caa6b8bb38cf6f9ce69f506b9cccdec0e00
mka <mka281@icloud.com>
2018-12-22 16:39:05 -0700

    Deploy to Rinkeby test network

--- smart_contracts/truffle.js
+++ smart_contracts/truffle.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const mnemonic = 'verify belt float only coach purity logic rural afford mouse lock blind'
+
 /*
  * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
  * function when declaring them. Failure to do so will cause commands to hang. ex:
@@ -22,10 +26,15 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     rinkeby: {
-      host: "localhost", //local node
-      port: 8545, // connection port
-      network_id: 4, // network id for test networks
-      gas: 4700000 // gas limit
+      provider: function() { 
+        return new HDWalletProvider(
+          mnemonic,
+          'https://rinkeby.infura.io/v3/821ffe8aed6d49ca82f636a17b017da6'
+          )
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
     }
   }
 };
